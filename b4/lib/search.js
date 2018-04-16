7af52af6af020d8f8a1800c8013376d7a1fe4d6e
victor hazbun <victorhazbun87@gmail.com>
2018-04-16 16:15:28 -0500

    Using a Promise with request

--- b4/lib/search.js
+++ b4/lib/search.js
@@ -12,7 +12,6 @@ module.exports = (app, elasticsearch) => {
    * Search for books by matching a particular field value.
    * Example: /api/search/books/authors/Twain
    */
-
   app.get('/api/search/books/:field/:query', (req, res) => {
 
     const elasticsearchReqBody = {
@@ -42,6 +41,50 @@ module.exports = (app, elasticsearch) => {
 
       res.status(200).json(elasticsearchResBody.hits.hits.map(({_source}) => _source));
     });
+  });
+
+
+  /**
+   * Collect suggested terms for a given field based on a given query.
+   * Example: /api/suggest/authors/lipman
+   */
+  app.get('/api/suggest/:field/:query', (req, res) => {
+    const elasticsearchReqBody = {
+      size: 0,
+      suggest: {
+        suggestions: {
+          text: req.params.query,
+          term: {
+            field: req.params.field,
+            suggest_mode: 'always',
+          },
+        }
+      }
+    };
+
+    const options = {url, json: true, body: elasticsearchReqBody};
+
+    const promise = new Promise((resolve, reject) => {
+      request.get(options, (err, elasticsearchRes, elasticsearchResBody) => {
+
+        if (err) {
+          reject({error: err});
+          return;
+        }
+
+        if (elasticsearchRes.statusCode !== 200) {
+          reject({error: elasticsearchResBody});
+          return;
+        }
+
+        resolve(elasticsearchResBody);
+      });
+    });
+
+    promise
+      .then(elasticsearchResBody => res.status(200).json(elasticsearchResBody.suggest.suggestions))
+      .catch(({error}) => res.status(error.status || 502).json(error));
+
   });
 
 };
