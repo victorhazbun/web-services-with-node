5dd65856e2a1406425fcc03a3a517f3a3dec5569
victor hazbun <victorhazbun87@gmail.com>
2018-04-16 17:56:22 -0500

    Putting a Book into a Bundle

--- b4/lib/bundle.js
+++ b4/lib/bundle.js
@@ -4,9 +4,9 @@
 'use strict';
 const rp = require('request-promise');
 
-module.exports = (app, es) => {
+module.exports = (app, elasticsearch) => {
 
-  const url = `http://${es.host}:${es.port}/${es.bundles_index}/bundle`;
+  const url = `http://${elasticsearch.host}:${elasticsearch.port}/${elasticsearch.bundles_index}/bundle`;
 
   /**
    * Create a new bundle with the specified name.
@@ -59,6 +59,56 @@ module.exports = (app, es) => {
 
     } catch (elasticsearchResErr) {
       res.status(elasticsearchResErr.statusCode || 502).json(elasticsearchResErr.error);
+    }
+  });
+
+
+  /**
+   * Put a book into a bundle by its id.
+   * curl -X PUT http://<host>:<port>/api/bundle/<id>/book/<pgid>
+   */
+  app.put('/api/bundle/:id/book/:pgid', async (req, res) => {
+    const bundleUrl = `${url}/${req.params.id}`;
+
+    const bookUrl =
+        `http://${elasticsearch.host}:${elasticsearch.port}` +
+        `/${elasticsearch.books_index}/book/${req.params.pgid}`;
+
+    try {
+
+      // Request the bundle and book in parallel.
+      /* const [bundleRes, bookRes] = await Promise.all([
+        rp({url: bundleUrl, json: true}),
+        rp({url: bookUrl, json: true}),
+      ]);*/
+
+      const bundleRes = await rp({url: bundleUrl, json: true});
+      const bookRes = await rp({url: bookUrl, json: true});
+
+
+      // Extract bundle and book information from responses.
+      const {_source: bundle, _version: version} = bundleRes;
+      const {_source: book} = bookRes;
+
+      const idx = bundle.books.findIndex(book => book.id === req.params.pgid);
+      if (idx === -1) {
+        bundle.books.push({
+          id: req.params.pgid,
+          title: book.title,
+        });
+      }
+
+      // Put the updated bundle back in the index.
+      const esResBody = await rp.put({
+        url: bundleUrl,
+        qs: { version },
+        body: bundle,
+        json: true,
+      });
+      res.status(200).json(esResBody);
+
+    } catch (esResErr) {
+      res.status(esResErr.statusCode || 502).json(esResErr.error);
     }
   });
 };
